083b83ccd31552fe9b3ccf7b6cdf4aa75fa6cf47
mkm <clownpiece7473@outlook.com>
2025-10-26 18:00:31 +0800

    move exceptions to jsonexception.h

---
+++ jsonexception.h
@@ -0,0 +1,45 @@
+//
+// Created on 2025/10/26.
+//
+
+#ifndef JSONPP_JSONEXCEPTION_H
+#define JSONPP_JSONEXCEPTION_H
+#include <stdexcept>
+
+namespace JSONpp
+{
+    /*
+     * JSON exceptions
+     */
+    class JSONParseError : public std::runtime_error
+    {
+    public:
+        static constexpr const char* UNPARSABLE_MESSAGE = "Unparsable character(s)";
+
+        JSONParseError(std::string const& msg):
+            std::runtime_error(msg) {}
+
+        JSONParseError(std::string const& msg, size_t pos):
+            std::runtime_error(msg + " at position " + std::to_string(pos)) {}
+    };
+
+    class JSONTypeError : public std::runtime_error
+    {
+    public:
+        JSONTypeError(std::string const& msg) :
+            std::runtime_error(msg) {}
+    };
+
+    class JSONEmptyDataError : public std::runtime_error
+    {
+    public:
+        static constexpr const char* EMPTY_DATA_MESSAGE = "json document is empty";
+        JSONEmptyDataError(std::string const& msg = EMPTY_DATA_MESSAGE) :
+            std::runtime_error(msg) {}
+    };
+    /*
+     * end JSON exceptions
+     */
+}
+
+#endif //JSONPP_JSONEXCEPTION_H

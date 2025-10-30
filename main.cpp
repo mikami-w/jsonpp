120c2f72e96abc6c2dda535f21cf65bfc1ad488f
mkm <clownpiece7473@outlook.com>
2025-10-30 22:10:58 +0800

    aycs

--- main.cpp
+++ main.cpp
@@ -2,119 +2,15 @@
 #include <iostream>
 #include <string>
 #include "jsonpp.h"
+#include "tests/test.h"
 #include "print.h"
 #include "print_ref.h"
 
 int main()
 {
-    // test_print_h();
-
-    std::cout << std::boolalpha;
-
-    std::string input(R"JSON(
-{
-  "type": "minecraft:entity",
-  "pools": [
-    {
-      "bonus_rolls": 0.0,
-      "entries": [
-        {
-          "type": "minecraft:item",
-          "functions": [
-            {
-              "add": false,
-              "count": {
-                "type": "minecraft:uniform",
-                "max": 1.0,
-                "min": -1.0
-              },
-              "function": "minecraft:set_count"
-            },
-            {
-              "count": {
-                "type": "minecraft:uniform",
-                "max": 1.0,
-                "min": 0.0
-              },
-              "function": "minecraft:looting_enchant"
-            }
-          ],
-          "name": "minecraft:coal"
-        }
-      ],
-      "rolls": 1.0
-    },
-    {
-      "bonus_rolls": 0.0,
-      "entries": [
-        {
-          "type": "minecraft:item",
-          "functions": [
-            {
-              "add": false,
-              "count": {
-                "type": "minecraft:uniform",
-                "max": 2.0,
-                "min": 0.0
-              },
-              "function": "minecraft:set_count"
-            },
-            {
-              "count": {
-                "type": "minecraft:uniform",
-                "max": 1.0,
-                "min": 0.0
-              },
-              "function": "minecraft:looting_enchant"
-            }
-          ],
-          "name": "minecraft:bone"
-        }
-      ],
-      "rolls": 1.0
-    },
-    {
-      "bonus_rolls": 0.0,
-      "conditions": [
-        {
-          "condition": "minecraft:killed_by_player"
-        },
-        {
-          "chance": 0.025,
-          "condition": "minecraft:random_chance_with_looting",
-          "looting_multiplier": 0.01
-        }
-      ],
-      "entries": [
-        {
-          "type": "minecraft:item",
-          "name": "minecraft:wither_skeleton_skull"
-        }
-      ],
-      "rolls": 1.0
-    }
-  ],
-  "random_sequence": "minecraft:entities/wither_skeleton"
-}
-)JSON");
-    if (auto json = JSONpp::parse(input); json.has_value())
-    {
-        // auto json_value = json.value();
-        JSONpp::JSONValue json_value("2222");
-        std::cout << json_value.stringify() << std::endl;
-    }
-    else
-    {
-        std::cout << "document is empty." << std::endl;
-    }
-
-
-    // JSONpp::JSONValue json("Hello World");
-    // std::cout << json.as_string() << std::endl;
-    // json = 123;
-    // std::cout << json.as_int64() << std::endl;
-    // json = (void*)0;
-    // json = {1, 1};
+    Test::test_usability();
+    std::cerr << std::endl;
+    Test::test_error_handling();
 
     return 0;
 }
---
+++ tests/test.h
@@ -0,0 +1,87 @@
+//
+// Created on 2025/10/30.
+//
+
+#ifndef JSONPP_TEST_H
+#define JSONPP_TEST_H
+#include <algorithm>
+#include <filesystem>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include "../jsonpp.h"
+
+namespace fs = std::filesystem;
+
+namespace Test
+{
+    static constexpr char const* testFilePath_usability = "d:/works/cpp/JSONpp/tests/usability/";
+    static constexpr char const* testFilePath_errorHandling = "d:/works/cpp/JSONpp/tests/error_handling/";
+
+    inline bool is_hidden(fs::directory_entry const& file)
+    {
+        return file.path().filename().string().front() == '.';
+    }
+
+    inline std::string readFileToString(std::string const& filename) {
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Error: Could not open file '" << filename << "'" << std::endl;
+            return "";
+        }
+
+        // 一次性构造字符串
+        //    - std::istreambuf_iterator<char>(file) : 指向文件流开始的迭代器
+        //    - std::istreambuf_iterator<char>()     : 默认构造函数，表示 "end-of-stream"
+        std::string content(
+            (std::istreambuf_iterator<char>(file)),
+            (std::istreambuf_iterator<char>())
+        );
+
+        file.close();
+
+        return content;
+    }
+
+    inline void test_usability()
+    {
+        std::cerr << "========== USABILITY TEST ==========" << std::endl;
+        for (auto const& file
+            : fs::directory_iterator(testFilePath_usability))
+        {
+            try
+            {
+                JSONpp::parse(readFileToString(file.path().string()));
+
+            } catch (std::exception& e)
+            {
+                std::cerr << "Error occurred in file " << file.path().filename() << std::endl;
+                std::cerr << e.what() << std::endl;
+            }
+        }
+        std::cerr << "========== USABILITY TEST END ==========" << std::endl;
+    }
+
+    inline void test_error_handling()
+    {
+        std::cerr << "========== ERROR HANDLING TEST ==========" << std::endl;
+        for (auto const& file
+            : fs::directory_iterator(testFilePath_errorHandling))
+        {
+            try
+            {
+                JSONpp::parse(readFileToString(file.path().string()));
+            } catch (std::exception& e)
+            {
+                std::cerr << "Error occurred in file " << file.path().filename() << std::endl;
+                std::cerr << e.what() << std::endl;
+            }
+        }
+        std::cerr << "========== ERROR HANDLING TEST END ==========" << std::endl;
+    }
+};
+
+
+#endif //JSONPP_TEST_H

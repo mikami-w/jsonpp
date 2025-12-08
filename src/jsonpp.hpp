d543878fb73c6c1ec879929cd6afd787c479bc2d
mikami <clownpiece7473@outlook.com>
2025-12-08 18:05:43 +0800

    fix(jsonpp.hpp): reorder includes for better organization and remove duplicates

--- src/jsonpp.hpp
+++ src/jsonpp.hpp
@@ -2,10 +2,10 @@
 #define JSONPP_JSONPP_HPP
 
 #include "macro_def.hpp"
+#include "traits.hpp"
+#include "jsonexception.hpp"
 #include "json_fwd.hpp"
 #include "parser.hpp"
 #include "basic_json_impl.hpp"
-#include "jsonexception.hpp"
-#include "traits.hpp"
 
 #endif //JSONPP_JSONPP_HPP
